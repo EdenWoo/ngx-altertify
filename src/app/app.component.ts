1ab433fc88a8a2dcfb553f2aa350e0f07c100a23
Eden <xiaoyumqyz@gmail.com>
2019-03-11 11:25:42 +1300

    example upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {NgxAlertifyService} from '../../projects/ngx-alertify/src/lib/ngx-alertify.service';
 
+declare let alertify: any;
+
 @Component({
   selector: 'ngal-root',
   templateUrl: './app.component.html',
@@ -8,8 +10,36 @@ import {NgxAlertifyService} from '../../projects/ngx-alertify/src/lib/ngx-alerti
 })
 export class AppComponent {
   constructor(private alertifyService: NgxAlertifyService) {
-    alertifyService.success('sucess');
   }
 
-  title = 'ngx-alertify-app';
+  success() {
+    this.alertifyService.success('success message');
+  }
+
+  warning() {
+    this.alertifyService.warning('warning message');
+  }
+
+  error() {
+    this.alertifyService.error('error message');
+  }
+
+  message() {
+    this.alertifyService.message('message message');
+  }
+
+  youtubeDialog() {
+    this.alertifyService.youtubeDialog();
+  }
+
+  alert() {
+    alertify.confirm('This is a confirm dialog.',
+      function () {
+        alertify.success('Ok');
+      },
+      function () {
+        alertify.error('Cancel');
+      });
+  }
+
 }
